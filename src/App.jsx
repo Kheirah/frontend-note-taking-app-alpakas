9b4ef94c73a205c44fcea67f4cd3c5c903825c6b
Ernst Stolz <eric@spiritsdontfly.com>
2024-05-16 15:18:19 +0200

    add functionality to add notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const url = "https://rest-and-expressjs-alpakas.onrender.com";
+/* const url = "https://rest-and-expressjs-alpakas.onrender.com"; */
+const localUrl = "http://localhost:3000";
 
 function App() {
   const [notes, setNotes] = useState([]);
   const [user, setUser] = useState("Max");
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     getNotes();
     async function getNotes() {
-      const res = await fetch(`${url}/${user}`);
+      const res = await fetch(`${localUrl}/${user}`);
       const data = await res.json();
-      console.log(data);
-      setNotes(data);
+      if ("message" in data) {
+        setMessage(data.message);
+        setNotes([]);
+      } else {
+        setNotes(data);
+        setMessage("");
+      }
     }
-  }, [url, user]);
+  }, [user]);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const contentValue = event.target.content.value;
+
+    const res = await fetch(`${localUrl}/${user}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ content: contentValue }),
+    });
+    event.target.reset();
+    const data = await res.json();
+    setMessage("message" in data ? data.message : "");
+  }
+
+  function handleDelete() {
+    //todo delete a note
+  }
 
   return (
     <>
@@ -26,11 +53,19 @@ function App() {
         <button onClick={() => setUser("Eric")}>Eric</button>
         <ul>
           {notes.map((note) => (
-            <li key={note.id}>{note.content}</li>
+            <li key={note.id}>
+              {note.content}
+              <button onClick={() => {}}>X</button>
+            </li>
           ))}
         </ul>
+        <form onSubmit={handleSubmit}>
+          <input id="content" type="text" />
+          <button>Add note</button>
+        </form>
       </div>
       <p className="read-the-docs">Response from the server</p>
+      <div>{message}</div>
     </>
   );
 }
